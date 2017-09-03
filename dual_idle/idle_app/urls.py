70d24cd08fb66be0cce01cbaa4b76bf04dce68a8
jalockie <jalockie@gmail.com>
2017-09-03 23:33:58 +1000

    add template for landing page

--- dual_idle/idle_app/urls.py
+++ dual_idle/idle_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'landing', views.landing, name='landing'),
 ]
--- dual_idle/idle_app/views.py
+++ dual_idle/idle_app/views.py
@@ -7,3 +7,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the idle index.")
+
+def landing(request):
+    return render(request, 'landing.html', {
+        "default_linking_code": "d8cd98f00b204e9"})
