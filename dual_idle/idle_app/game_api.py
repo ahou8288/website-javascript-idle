d1620c2573b775f21a12c51cb146217df868d41f
jalockie <jalockie@gmail.com>
2017-10-01 23:39:09 +1100

    add sha1 hash as game linking code

--- dual_idle/idle_app/game_api.py
+++ dual_idle/idle_app/game_api.py
@@ -1,18 +1,25 @@
 from idle_app import models
+from django.contrib.auth import get_user
 from django.core.serializers.json import DjangoJSONEncoder as JSON
+import hashlib
+from datetime import datetime
 
 def create_game(request):
-    user = request.user
+    user = get_user(request)
+    game_id_str = "%s%d"%(user.email,datetime.now().microsecond)
+    link_code = hashlib.sha1(game_id_str.encode('utf-8')).hexdigest()
     game = models.Game(
         creationDate="1970-06-06",
-        linkingCode="",
+        linkingCode=link_code,
         isPublic=True,
-    ).save()
+    )
+    game.save()
     userGame = models.UserGame(
         user=user,
         game=game,
         wealth=0,
-    ).save()
+    )
+    userGame.save()
     if request.META['CONTENT_TYPE'] == 'json':
         # In case of restful json api endpoint
         return JSON.encode(userGame)
