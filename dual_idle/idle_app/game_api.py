9c95b85c77f4e503d33fca5bd9d8274e214963d2
jalockie <jalockie@gmail.com>
2017-10-09 03:31:06 +1100

    update the game state update post request handler

--- dual_idle/idle_app/game_api.py
+++ dual_idle/idle_app/game_api.py
@@ -3,7 +3,8 @@ from django.contrib.auth import get_user
 from django.core.serializers.json import DjangoJSONEncoder as JSONEncoder
 import json
 import hashlib
-from datetime import datetime
+from datetime import datetime, timedelta
+from django.http import HttpResponse
 
 def create_game(request):
     user = get_user(request)
@@ -21,14 +22,41 @@ def create_game(request):
         wealth=0,
     )
     userGame.save()
-    print(userGame.game)
     if request.META['CONTENT_TYPE'] == 'json':
+        pass
         # In case of restful json api endpoint
-        return JSONEncoder.encode(userGame)
+        # return JSONEncoder.encode(userGame)
     return userGame
 
-def update_user_game(request):
+def update_players_items(g_o, game, user, ctx):
+    myItems = models.PlayerItem.objects.filter(game=game, user=user)
+    for updated_item in g_o.get(ctx):
+        item=None
+        try:
+            item = models.Item.objects.get(name=updated_item['item']['name'])
+        except Exception:
+            item = models.Item(upgradeValue=updated_item['item']['upgradeValue'],
+                               baseValue=updated_item['item']['baseValue'],
+                               name=updated_item['item']['name'],
+                               displayImagePath='')
+            item.save()
+        myItem=None
+        try:
+            myItem = myItems.get(item=item)
+            myItem.upgradeQuantity = updated_item['upgradeQuantity']
+            myItem.quantity = updated_item['quantity']
+        except Exception:
+            myItem = models.PlayerItem(item=item,
+                                        user=user,
+                                        game=game,
+                                        quantity=updated_item['quantity'],
+                                        upgradeQuantity=updated_item['upgradeQuantity'])
+        myItem.save()
+    return [
+        it.item.__todict__() for it in models.PlayerItem.objects.filter(game=game, user=user)
+    ]
 
+def update(request):
     """request.body.game_object =
     {
         UserGame: {},
@@ -36,46 +64,9 @@ def update_user_game(request):
         partnerItems: {} this should only be newly purchased items for your partner!
     }
     """
-
-    request.body = {
-                        "userGame": {
-                            "user": {
-                                "displayName": "john",
-                                "id": 1
-                            },
-                            "wealth": 0,
-                            "game": {
-                                "id": 1
-                            },
-                            "mined": 0,
-                            "timePlayed": 0,
-                        },
-                        "playerItems": [
-                            {
-                                "item": {
-                                    "name": "calculator",
-                                    "baseValue": 0,
-                                    "upgradeValue": 0
-                                },
-                                "quantity": 1,
-                                "upgradeQuantity": 1
-                            },
-                        ],
-                        "partnerItems": [
-                            {
-                                "item": {
-                                    "name": "computer",
-                                    "baseValue": 0,
-                                    "upgradeValue": 0
-                                },
-                                "quantity": 1,
-                                "upgradeQuantity": 1
-                            },
-                        ]
-                    }
     if type(request.body) == bytes:
-        g_o = json.loads(request.body.decode('utf-8'))
-    else:
+        g_o = json.loads(request.POST.get("data", "0"))
+    elif type(request.body) == str:
         g_o = json.loads(json.dumps(request.body))
 
     user=None
@@ -88,59 +79,50 @@ def update_user_game(request):
             user = models.UserProfile.objects.get(id=g_o.get('userGame')['user']['id'])
             if not user:
                 raise Exception("Read the User library documentation :(")
+        elif user.is_anonymous:
+            err = {"error": "User does not exist"}
+            print(err)
+            return HttpResponse(json.dumps(err),
+                            content_type='application/json')
+    game=None
+    try:
+        game = models.Game.objects.get(id=g_o.get('userGame')['game']['id'])
+    except Exception:
+        err = {"error": "game does not exist"}
+        print(err)
+        return HttpResponse(json.dumps(err),
+                            content_type='application/json')
 
-    game = models.Game.objects.get(id=g_o.get('userGame')['game']['id'])
+    try:
+        myUserGame = models.UserGame.objects.get(game=game, user=user)
+        myUserGame.wealth = g_o.get('userGame')['wealth']
+        myUserGame.mined = g_o.get('userGame')['mined']
+        myUserGame.timePlayed = timedelta(seconds=g_o.get('userGame')['timePlayed'])
+        myUserGame.save()
+    except Exception:
+        err = {"error": "User is not assigned to this game"}
+        print(err)
+        return HttpResponse(json.dumps(err),
+                            content_type='application/json')
 
-    myUserGame = models.UserGame.objects.filter(game=game, user=user)
-    myUserGame.wealth = g_o.get('userGame')['wealth']
-    myUserGame.mined = g_o.get('userGame')['mined']
-    myUserGame.timePlayed = g_o.get('userGame')['timePlayed']
-    myUserGame.save()
+    update_players_items(g_o, game, user, 'playerItems')
 
-    myItems = models.PlayerItems.objects.filter(game=game, user=user)
-
-    for updated_item in g_o.get('playerItems'):
-        item = myItems.filter(name=updated_item['name'])
-        if not len(item):
-            if not len(models.Item.objects(name=updated_item['item']['name'])):
-                item = models.Item(upgradeValue=updated_item['item']['upgradeValue'],
-                                   baseValue=updated_item['item']['baseValue'],
-                                   name=updated_item['item']['name'],
-                                   displayImagePath='')
-                item.save()
-            item = models.PlayerItem(item=item.get(),
-                                     user=user,
-                                     quantity=updated_item['quantity'],
-                                     upgradeQuantity=updated_item['upgradeQuantity'])
-        else:
-            item.upgradeQuantity = updated_item['upgradeQuantity']
-            item.quantity = updated_item['quantity']
-        item.save()
-
-
-    partner = models.UserGame.objects.filter(game=game).exclude(user=user)
-    partnerUserGame = models.UserGame.objects.filter(user=partner)
+    partnerUserGame={}
     partnerItems = []
-    if len(partnerUserGame):
-        partnerItems = models.PlayerItems.objects.filter(game=game, user=partner)
-    else:
-        partnerUserGame = {}
-
+    partner = models.UserGame.objects.filter(game=game).exclude(user=user)
+    if len(partner):
+        if len(partner) > 1:
+            print("Too many users in this game!")
+        partner = partner[0].user
+        partnerUserGame = models.UserGame.objects.get(user=partner, game=game)
+        partnerItems = update_players_items(g_o, game, partner, 'partnerItems')
 
     """
         The response object body:
     """
-    game_object = {
-        "partnerUserGame": partnerUserGame,
+    response_object = {
+        "partnerUserGame": partnerUserGame.__todict__(),
         "partnerItems": partnerItems,
     }
-
-    if request.META['CONTENT_TYPE'] == 'json':
-        # In case of restful json api endpoint
-        return JSONEncoder.encode(game_object) # Todo: HttpResponse object
-    return game_object
-
-# class dummyrequest:
-#     body = None
-#
-# update_user_game(dummyrequest())
+    print(response_object)
+    return HttpResponse(json.dumps(response_object))
--- dual_idle/idle_app/urls.py
+++ dual_idle/idle_app/urls.py
@@ -9,9 +9,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'landing', views.landing, name='landing'),
     url(r'^game/posttest$', views.posttest, name='posttest'),
+    url(r'^game/update$', game_api.update, name='update'),
     url(r'^game/(?P<linkingCode>[a-zA-Z0-9]+)', views.game, name='game'),
     url(r'^login/$', views.login_view, name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^game_state/$', game_api.update_user_game, name='game_state'),
 ]
